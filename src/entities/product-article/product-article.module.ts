b83c408cc522bf23365d17a16726c85692fd9c85
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-10-13 20:03:43 +0300

    +

--- src/entities/product-article/product-article.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ProductArticleService } from './product-article.service';
-import { ProductArticleController } from './product-article.controller';
-
-@Module({
-  controllers: [ProductArticleController],
-  providers: [ProductArticleService],
-})
-export class ProductArticleModule {}
