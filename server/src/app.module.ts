3320e774548a155e418437b9d5703277766bb132
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-11-15 17:24:38 +0200

    +

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '@entities/auth/auth.module';
 import { UserModule } from '@entities/user/user.module';
+import { WarehoseModule } from './warehose/warehose.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UserModule],
+  imports: [ConfigModule.forRoot(), AuthModule, UserModule, WarehoseModule],
 })
 export class AppModule {}
