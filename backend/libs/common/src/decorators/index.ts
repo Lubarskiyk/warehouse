dfbab233530c0b661d5297940232f70d339885c3
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2025-05-21 13:18:53 +0300

    update prisma shema

--- backend/libs/common/src/decorators/index.ts
+++ backend/libs/common/src/decorators/index.ts
@@ -1,5 +1,5 @@
 export * from './is-passwords-matching-constraint.decorator';
 export * from './cookies.decorator';
-export * from './public.decortor';
+export * from './public.decorator';
 export * from './current-user.decorator';
 export * from './roles.decorator';
--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,15 +1,30 @@
-import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  INestApplication,
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
   }
-  async enableShutdownHooks(app: INestApplication) {
-    // @ts-ignore
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  enableShutdownHooks(app: INestApplication) {
+    (this.$on as (event: 'beforeExit', callback: () => any) => void)(
+      'beforeExit',
+      async () => {
+        await app.close();
+      },
+    );
   }
 }
